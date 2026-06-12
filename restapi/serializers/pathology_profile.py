1b5d8f11d581925f59349be5e54723ad404df6db
supreetashi <supreetashi11@gmail.com>
2026-06-12 11:52:14 +0530

    fix: category-test relationship cleanup and migration update

--- restapi/serializers/pathology_profile.py
+++ restapi/serializers/pathology_profile.py
@@ -4,9 +4,15 @@ from restapi.models.pathology_profile import Pathology_profile
 class PathologyProfileSerializer(serializers.ModelSerializer):
     
     clinic_name = serializers.CharField(
-    source="clinic.clinic_name",
-    read_only=True
+        source="clinic.clinic_name",
+        read_only=True
     )
+
+    no_of_tests = serializers.SerializerMethodField()
+
     class Meta:
         model = Pathology_profile
         fields = '__all__'
+
+    def get_no_of_tests(self, obj):
+        return obj.tests.count()
